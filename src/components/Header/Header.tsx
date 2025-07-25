b319e2b0eb9d67d61d026dde82ed483efc9418f4
Stanislav <ssovelich@gmail.com>
2025-07-25 12:33:17 +0300

    Update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,35 @@
+import styles from "./Header.module.scss";
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div>
+        <p>G112082</p>
+        <button type="button">
+          <img src="/icons/edit.svg" alt="Edit" />
+          Edit
+        </button>
+      </div>
+      <div className={styles.wrapBtn}>
+        <button type="button">
+          <img src="/icons/arrow-prev.svg" alt="Arrow prev" />
+        </button>
+        <button type="button">
+          <img
+            className={styles.icon}
+            src="/icons/arrow-next.svg"
+            alt="Arrow next"
+          />
+        </button>
+        <button type="button">
+          <img src="/icons/maximize.svg" alt="Maximize" />
+        </button>
+        <button type="button">
+          <img src="/icons/close.svg" alt="Close" />
+        </button>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
